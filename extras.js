476a07dfc6b59751940022ed287db78ec8ac6749
PkSM3 <pokesam3@gmail.com>
2013-12-09 16:06:44 -0300

    new tab names for submods

--- extras.js
+++ extras.js
@@ -12,7 +12,7 @@ function callGeomap(){
         jsonparams = jsonparams.split('&').join('__and__');
     }    
     pr('in callGeomap: db='+db+'&query='+jsonparams);
-    initiateMap(db,jsonparams);
+    initiateMap(db,jsonparams,"geomap/");
 }
 
 function getTopPapers(type){
