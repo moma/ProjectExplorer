7ee9aab7633433248431b935151c180260f0838f
PkSM3 <pokesam3@gmail.com>
2016-01-17 23:47:03 +0100

    etoile

--- LOCALDB/default_div.php
+++ LOCALDB/default_div.php
@@ -294,10 +294,10 @@ function imagestar($score,$factor,$twjs) {
 	if ($score > .5) {
 		$star_image = '';
 		for ($s = 0; $s < min(5,$score/$factor); $s++) {
-			$star_image.='<img src="'.$twjs.'img/star.gif" border="0" >';
+			$star_image.='<img src="'.'img/star.gif" border="0" >';
 		}
 	} else {
-		$star_image.='<img src="'.$twjs.'img/stargrey.gif" border="0">';
+		$star_image.='<img src="'.'img/stargrey.gif" border="0">';
 	}
 	return $star_image;
 }
