d767450c18b1b85c928ab1da78fe4597729d679e
rloth <romain.loth@iscpif.fr>
2017-09-26 13:55:16 +0200

    typo

--- twmain/main.js
+++ twmain/main.js
@@ -446,7 +446,7 @@ function mainStartGraph(inFormat, inData, twInstance) {
       TW.graphData = {nodes: [], edges: []}
       TW.graphData = sigma_utils.FillGraph( initialActivetypes , initialActivereltypes, TW.catDict  , TW.Nodes , TW.Edges , TW.graphData );
 
-      if (TW.graphData.edges.length == 0) console.warning("no edges in graph")
+      if (TW.graphData.edges.length == 0) console.warn("no edges in graph")
       if (TW.graphData.nodes.length == 0) {
         console.error("empty graph")
         alert("This query returned no nodes. The graph is empty")
