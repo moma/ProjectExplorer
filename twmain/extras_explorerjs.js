42740295ae1fc2edab71a9f91db9f296235abf4f
rloth <romain.loth@iscpif.fr>
2017-10-23 18:02:46 +0200

    fix another bug with legend clearing that prevented resetGraph and standalone input opening

--- twmain/extras_explorerjs.js
+++ twmain/extras_explorerjs.js
@@ -338,7 +338,6 @@ function graphResetAllColors() {
 
 // removes selectively for an array of nodetypes
 function clearColorLegend (forTypes) {
-  // console.log('clearColorLegend', forTypes)
   for (var ty of forTypes) {
     let legTy = document.getElementById("legend-for-"+TW.catDict[ty])
     if (legTy)
