5d85b924715874ca688f70c71f2c7c4529b9d57f
rloth <romain.loth@iscpif.fr>
2017-11-17 09:48:39 +0100

    stable comex presets

--- twpresets/settings_explorerjs.comex.js
+++ twpresets/settings_explorerjs.comex.js
@@ -190,12 +190,14 @@ TW.conf = (function(TW){
 
   TWConf.colorByAtt = true;            // show "Set colors" menu
 
-  TWConf.tuningPanel = true;           // show "Tune settings" menu button
+  TWConf.tuningPanel = false;          // show "Tune settings" menu button
 
   TWConf.dragNodesAvailable = true;    // allow dragging nodes with CTRL+click
 
-  TWConf.deselectOnclickStage = true   // click on background remove selection ?
+  TWConf.deselectOnClickStage = false   // click on background remove selection?
                                        // (except when dragging)
+
+  TWConf.deselectOnDoubleClickStage = true   // idem double click on background
 
   TWConf.histogramStartThreshold = 10 ;   // for daily histo module
                                           // (from how many docs are significant)
@@ -244,7 +246,7 @@ TW.conf = (function(TW){
 
   TWConf.independantTypes = true   // if stablePositions, types are not moving together
 
-  TWConf.colorTheme = "24DivergingZeileis"   // color palette for clusters
+  TWConf.colorTheme = "24ContrastedPastel"   // color palette for clusters
                                              //  - "9CBrewerSet1"
                                              //  - "12CBrewerPaired",
                                              //  - "22Kelly"
@@ -281,7 +283,7 @@ TW.conf = (function(TW){
       twDefaultBGBoxColor: '#fff',      // common value for hovered/selected
                                         //               label box bg def color
       defaultHoverLabelColor: '#000',
-      borderSize: 2.5,                   // for ex, bigger border when hover
+      borderSize: 1,                     // for ex, bigger border when hover
       nodeBorderColor: "node",           // choices: 'default' color vs. node color
       defaultNodeBorderColor: "black",   // <- if nodeBorderColor = 'default'
       labelHoverBGColor: "default",     // "node" for a label bg like the node color,
